41df09a597a5febb4af4c8a5acc8e5f0acf85720
Mark Stover <mstover@gospotcheck.com>
2019-03-08 14:52:19 -0700

    add test and sum function for slices

---
+++ arrayAndSlices/arrayAndSlices_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
